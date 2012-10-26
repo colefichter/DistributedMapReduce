c2c97440ec640139c83a2bd66e7ba30317a6b155
Cole Fichter <cole.fichter@gmail.com>
2012-10-26 15:47:34 -0600

    Added median function and refactored.

--- src/compute.erl
+++ src/compute.erl
@@ -3,7 +3,7 @@
 -compile([export_all]).
 
 % Sample map-reduce algorithms --------------------------------
-count() -> %Count the number of integers stored in the system
+count() ->
     Map = fun(_X) -> 1  end,
     Reduce = fun(List) -> length(List) end,
     mrs:mapreduce(Map, Reduce).
@@ -13,25 +13,42 @@ sum() -> %Find the sum of all the integers stored in the system
     Reduce = fun lists:sum/1,
     mrs:mapreduce(Map, Reduce).
 
-max() -> %Find the largest integer stored in the system 
-    Map = fun(X) -> X end,
-    Reduce = fun lists:max/1,
-    mrs:mapreduce(Map, Reduce).
-
 min() -> %Find the smallest integer stored in the system
     Map = fun(X) -> X end,
     Reduce = fun lists:min/1,
+    mrs:mapreduce(Map, Reduce).
+
+max() -> %Find the largest integer stored in the system 
+    Map = fun(X) -> X end,
+    Reduce = fun lists:max/1,
     mrs:mapreduce(Map, Reduce).
 
 mean() ->
     Map = fun(X) -> X end,
     Reduce = fun(List) ->
 		     {Sum, Count} = lists:foldl(fun(X, {Sum0, Count0}) ->
-						{Sum0 + X, Count0 + 1}	
+							{Sum0 + X, Count0 + 1}	
 						end, {0,0}, List),
 		     Sum / Count
 	     end,
-    mrs:mapreduce(Map, Reduce).		  
+    mrs:mapreduce(Map, Reduce).
+
+median() ->		  
+    Map = fun(X) -> X end,
+    Reduce = fun(List) ->
+		     List1 = lists:sort(List),
+		     N = length(List1),
+		     I = round(N / 2),
+		     Median = case (N rem 2) of 
+				  1 ->
+				      lists:nth(I, List1);
+				  0 ->
+				      Sum = lists:nth(I, List1) + lists:nth(I+1, List1),
+				      Sum / 2
+			      end,
+		     Median
+	     end,
+     mrs:mapreduce(Map, Reduce).
 
 most_common() -> %Find the most common integer stored in the system, and the number of times it occurs.
     Map = fun(X) -> X end,
